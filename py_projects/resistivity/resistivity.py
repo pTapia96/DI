19027192b91a5afdf4ae73e7a89bbbc224ff46f0
pTapia96 <pabrod@alu.iesserpis.org>
2019-09-18 12:36:24 +0200

    Resistivity exercise before correction

--- py_projects/resistivity/resistivity.py
+++ py_projects/resistivity/resistivity.py
@@ -1,12 +1,17 @@
 def resistivity  (r: float, a: float, l: float) -> float:
-    """Return the electrical resistivity of a specimen with r electrical resistance,
-    l length and a cross-sectional area
-
+    """Calculates electrical resistivity of a uniform specimen.
+    
+    Parameters:
+    r: electrical resistance of the specimen
+    a: cross-sectional area of the specimen
+    l: lenght of the specimen
+    
     Preconditions:
     r > 0
     a > 0
     l > 0
-
+    
+    Examples:
     >>> resistivity(5, 5, 5)
     5.0
     >>> resistivity(240, 30.31, 14.3)
@@ -14,6 +19,7 @@ def resistivity  (r: float, a: float, l: float) -> float:
     """
     return round(r*(a/l), 2)
 
-
-import doctest
-doctest.testfile("doctest.txt")
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    doctest.testfile("doctest.txt")
